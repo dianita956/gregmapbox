bdf44abd82c4eb68613d11e8f7e004cf2ce323b8
Diane Lopez <70670592+dianita956@users.noreply.github.com>
2024-11-19 17:26:55 -0600

    added data and index file

---
+++ data.js
@@ -0,0 +1,105 @@
+const data = {
+    "type": "FeatureCollection",
+    "name": "gdf",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "address": "Karnes County, Texas",
+                "Latitude": 28.8791858,
+                "Longitude": -97.9062893
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -97.9062893,
+                    28.8791858
+                ]
+            },
+            "content": {
+                "stanza": "En el condad de Carnes. \nmiren lo que ha sucedido,\nmurió el Cherife Mayor. \nquednado Roman herido"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "address": "Gonzales County, Texas",
+                "Latitude": 29.4436555,
+                "Longitude": -97.5108636
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -97.5108636,
+                    29.4436555
+                ]
+            },
+            "content":{
+                "stanza":"Tiró con rumbo a Gonzales \nsin ninguna timidez:\n--Síganme rinches cobardes, \nyo soy Gregorio Cortez."
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "address": "Belmont, Gonzalez County, Texas",
+                "Latitude": 29.5241066,
+                "Longitude": -97.6875125
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -97.6875125,
+                    29.5241066
+                ]
+            },
+            "content": {
+                "stanza": "Se fue de Belmont al rancho, \nlo alacanzaron a rodear, \npoquitos más de trescientos, \ny allí les brincó el corral.",
+            
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "address": "Laredo, Texas",
+                "Latitude": 27.5075005,
+                "Longitude": -99.5069922
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -99.5069922,
+                    27.5075005
+                ]
+            },
+            "content": {
+                "stanza": "Salió Gregorio Cortez, \nsalió con rumbo a Laredo, \nno lo quisieron seguir \nporque le tuvieron miedo.",
+            
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "address": "Encinal, Texas",
+                "Latitude": 28.0406329,
+                "Longitude": -99.3552096
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -99.3552096,
+                    28.0406329
+                ]
+            },
+            "content": {
+                "stanza": "Allá por El Encinal, \nsegún lo que aquí se dice, \nle formaron un corral y les mató otro cherife.",
+                
+            }
+        }
+    ]
+}
